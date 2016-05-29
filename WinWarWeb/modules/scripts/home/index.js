c43e4bdb75f73ad9d76e2d009e992ba8b6d6d874
muge <xiaoxiaomuge@163.com>
2016-05-29 19:25:43 +0800

    初始项目

--- WinWarWeb/modules/scripts/home/index.js
+++ WinWarWeb/modules/scripts/home/index.js
@@ -6,10 +6,20 @@
     };
 
     ObjectJS.bindEvent = function () {
+        $(".nav li .nav-item").click(function () {
+            if (!$(this).hasClass("active")) {
+                $(this).parent().siblings().find(".nav-item").removeClass("active");
+                $(this).addClass("active");
+            }
+        });
+
         $(".menu li").click(function () {
-            $(this).addClass("menu-active");
+            if (!$(this).hasClass("active")) {
+                $(this).siblings().removeClass("active").find(".iconfont").css("color", "#666");
+                $(this).addClass("active").find(".iconfont").css("color", "#4A98E7");
+            }
         });
     };
 
-    exports.module = ObjectJS;
+    module.exports= ObjectJS;
 });

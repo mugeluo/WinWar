54b78deb38fa333e46f30964fbb685a7687eaa1d
muge <xiaoxiaomuge@163.com>
2016-05-29 18:46:21 +0800

    首页原型

---
+++ WinWarWeb/modules/scripts/home/index.js
@@ -0,0 +1,15 @@
+﻿define(function (require, exports, module) {
+    var ObjectJS={};
+
+    ObjectJS.init = function () {
+        ObjectJS.bindEvent();
+    };
+
+    ObjectJS.bindEvent = function () {
+        $(".menu li").click(function () {
+            $(this).addClass("menu-active");
+        });
+    };
+
+    exports.module = ObjectJS;
+});

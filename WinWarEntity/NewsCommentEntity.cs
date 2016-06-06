b9d0885901b60eaa4e1f6194d1826dd7f7858157
sunallen521 <sunallen@163.com>
2016-06-06 21:39:55 +0800

    逻辑层结构和实体

---
+++ WinWarEntity/NewsCommentEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinWarEntity
+{
+    public class NewsCommentEntity
+    {
+        public int ID { get; set; }
+
+        public int User_ID { get; set; }
+
+        public string User_Name { get; set; }
+
+        public int News_Uni_Code { get; set; }
+
+        public int Type { get; set; }
+
+        public string Content { get; set; }
+
+        public int Praise_Count { get; set; }
+
+        public int Reply_Count { get; set; }
+
+        public int Reply_ID { get; set; }
+
+        public int Reply_User_ID { get; set; }
+
+        public string Reply_User_Name { get; set; }
+
+        public DateTime Create_Date { get; set; }
+    }
+}
---
+++ WinWarEntity/NewsEntity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinWarEntity
+{
+    public class NewsEntity
+    {
+        public int News_Uni_Code { get; set; }
+
+        public DateTime Pub_Time { get; set; }
+
+        public string News_Author { get; set; }
+
+        public int Nega_Post_Par { get; set; }
+
+        public string Title_Main { get; set; }
+
+        public string Title_Sub { get; set; }
+
+        public string Title_App { get; set; }
+
+        public int News_Type { get; set; }
+
+        public int Source_Uni_Code { get; set; }
+
+        public string News_Sum { get; set; }
+
+        public string Is_Headline_Par { get; set; }
+
+        public int Impt_Par { get; set; }
+
+        public string Is_Main_News { get; set; }
+
+        public string Is_Issue { get; set; }
+
+        public string Real_Source_Name { get; set; }
+
+        public string Pic_URL { get; set; }
+
+        public int View_Count { get; set; }
+
+        public int Comment_Count { get; set; }
+
+        public int Praise_Count { get; set; }
+
+        public int Collect_Count { get; set; }
+
+        public string Txt_Content { get; set; }
+
+        public string Html_Txt { get; set; }
+    }
+}
